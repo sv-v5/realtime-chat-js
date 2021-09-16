d9814b89239a8d1cf04ddefb3fee23dad9502043
sv-v5 <svyatver@gmail.com>
2021-09-16 13:27:13 -0500

    test ws chat, user join and leave. new message notifications when scrolled up

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+// TODO: heroku address and PORT
 const socket = io('http://localhost:3000')
+const messageForm = document.getElementById('send-container');
+const messageContainer = document.getElementById('message-container');
+const messageInput = document.getElementById('message-input');
 
-socket.on('chat-message', data => {console.log(data)} );
+const username = prompt('Enter your username');
+socket.emit('user-join', username);
+
+// msg sent from server with type or identifier 'chat-message'
+socket.on('chat-message', data => {appendMessage(data)} );
+
+messageForm.addEventListener('submit', e => {
+    // prevent default behavior of submitting form (opening a file behavior?). also prevents page refresh
+    e.preventDefault();
+    const message = messageInput.value;
+    socket.emit('send-chat-message', message);
+    messageInput.value = '';
+})
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    // always scroll to the bottom. scrollTop is the amount of scrolling done on the element/container
+    // https://stackoverflow.com/questions/25505778/automatically-scroll-down-chat-div
+    var atbottom = false;
+    if (messageContainer.scrollTop + messageContainer.clientHeight > messageContainer.scrollHeight -1) {
+        atbottom = true;  // tolerate 0.5 units of height diff. sometimes chat divs are 'cut off'
+    }
+    messageContainer.append(messageElement);
+    // add logic to show notice if there are new messages, if the user is not scrolled all the way down
+    newMsgNotice = document.getElementById('new-message-notice');
+    if (atbottom) {
+        messageContainer.scrollTop = messageContainer.scrollHeight;
+        newMsgNotice.style['visibility'] = 'hidden';
+    } else {
+        newMsgNotice.style['visibility'] = 'visible';
+    }
+}
--- server.js
+++ server.js
@@ -1,3 +1,25 @@
+// for heroku use process.env.PORT || 3000
 const io = require('socket.io')(3000)
 
-io.on('connection', socket => {socket.emit('chat-message', 'Hello chat')});
+// store user names when a connection is made and a user-join msg is received from script.js
+const users = {};
+
+io.on('connection', socket => {
+    // socket.emit('chat-message', 'Hello chat');
+    socket.on('user-join', username => {
+        users[socket.id] = username;
+        // template strings, like python's fstrings `here is a variable ${var}`
+        io.emit('chat-message', (`${username} has joined the chat`));
+    });
+    // receive chat message from clients
+    socket.on('send-chat-message', message => {
+        // console.log(message);
+        io.emit('chat-message', `${users[socket.id]}: ${message}`);
+        // TODO: give users a color to differentiate between messages
+    });
+    socket.on('disconnect', () => {
+        io.emit('chat-message', `${users[socket.id]} has left the chat`);
+        delete users[socket.id];
+        // io.emit('chat-message', `size of users ${Object.keys(users).length}`);  // track number of users in the chat
+    });
+});
